cd08e424840a7f1d4ad16b3a7663cbebe5ae7941
Randy Ferrer <randfer@gmail.com>
2019-10-17 10:48:07 -0400

    Added database support, middleware and rendered total day count to page

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GraphQLString } from 'graphql';
+import { Query } from 'react-apollo';
 import{
+  DataLayer,
+  Entry,
   Environment,
   IsomorphicApp,
+  Middleware,
   Route,
-  WebApp
+  serviceWithDataLayer,
+  update,
+  WebApp,
+  withDataLayer,
+  withIsoMorphicState
 } from 'infrastructure-components';
+
+const setDate = (d, hours) => (
+  new Date(d.getFullYear(), d.getMonth(), d.getDate(), hours)
+);
+
+const pad = (n) => n < 10 ? '0'+n : n;
+
+const utcstring = (d) => (
+  d.getUTCFullYear()
+    + "-" + pad(d.getUTCMonth() + 1)
+    + "-" + pad(d.getUTCDate())
+    + "-" + pad(d.getUTCHours())
+);
 
 export default (
   <IsomorphicApp
@@ -13,20 +35,66 @@ export default (
     assetsPath='assets'
     region='us-east-1'>
 
-      <Environment name='dev' />
+    <Environment name='dev' />
 
-      <WebApp
-        id='main'
-        path='*'
-        method='GET'>
+    <DataLayer id='datalayer'>
+        <Entry
+          id='visitentry'
+          primaryKey='visittimestamp'
+          data={{ visitcount: GraphQLString }}
+        />
 
-        <Route
-          path='/'
-          name='React-Architect'
-          render={(props) => (
-            <div>Hello React-Architect!</div>
-          )} />
+        <WebApp
+          id='main'
+          path='*'
+          method='GET'>
 
-      </WebApp>
+          <Route
+            path='/'
+            name='React-Architect'
+            render={withDataLayer((props) => (
+              <div>
+                <Query {...props.getEntryScanQuery('visitentry', {
+                  visittimestamp: [
+                    utcstring(setDate(new Date(), 0)),
+                    utcstring(setDate(new Date(), 23))
+                  ]
+                })} >
+                {
+                  ({loading, data, error}) => (
+                      loading && <div>Calculating...</div>
+                    ) || (
+                      data && <div>Total Visitors Today: {
+                        data['scan_visitentry_visittimestamp'].reduce(
+                          (total, entry) => total + parseInt(entry.visitcount), 0
+                          )
+                      }</div>
+                    ) || (
+                      <div>Error loading data</div>
+                    )
+                }
+                </Query>
+              </div>
+            ))}>
+
+            <Middleware callback={serviceWithDataLayer(
+
+              async function(datalayer, request, response, next) {
+
+                await update(
+                  datalayer.client,
+                  datalayer.updateEntryQuery('visitentry', data => ({
+                    visittimestamp: utcstring(new Date()),
+                    visitcount: data.visitcount ?
+                      parseInt(data.visitcount) + 1 : 1
+                  }))
+                );
+                next();
+              }
+            )}/>
+
+          </Route>
+        </WebApp>
+      </DataLayer>
     </IsomorphicApp>
 );
